95f9ff04418b79d4caa8a0ea7189492c20d92888
berkayyilmaz <berkayyilmaz@gmail.com>
2021-12-16 15:56:24 +0300

    Fix null object referance on getActivity

--- android/src/main/java/com/mocklocation/reactnative/RNMockLocationDetectorModule.java
+++ android/src/main/java/com/mocklocation/reactnative/RNMockLocationDetectorModule.java
@@ -1,6 +1,7 @@
 package com.mocklocation.reactnative;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -31,15 +32,21 @@ public class RNMockLocationDetectorModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void checkMockLocationProvider(final Promise promise) {
-        if (ActivityCompat.checkSelfPermission(getCurrentActivity(),
+        final Activity activity = getCurrentActivity();
+
+        if(activity == null){
+            promise.reject("00001", "Activity is null");
+        }
+
+        if (ActivityCompat.checkSelfPermission(activity,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(getCurrentActivity(),
+            ActivityCompat.checkSelfPermission(activity,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             promise.resolve("PERMISSION_REJECTED");
         }
         FusedLocationProviderClient mFusedLocationClient;
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getCurrentActivity());
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(getCurrentActivity(),
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(activity,
             new OnSuccessListener < Location > () {
                 @Override
                 public void onSuccess(Location location) {
